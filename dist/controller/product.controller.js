e6a01d623ab7a9b30510d05d776f9ad907bbca6a
phanusonboss <phanusonbosslove@gmail.com>
2024-09-18 13:45:27 +0700

    first commit

---
+++ dist/controller/product.controller.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const product_1 = __importDefault(require("../db/product"));
+const getAll = (req, res) => {
+    product_1.default
+        .selectAll() //--db/product.ts
+        .then((products) => {
+        // .then for async call
+        res.status(200).send({
+            message: "OK",
+            result: products,
+        });
+    })
+        .catch((err) => {
+        res.status(500).send({
+            message: "DATABASE ERROR",
+            error: err.code,
+        });
+    });
+};
+exports.default = { getAll };
